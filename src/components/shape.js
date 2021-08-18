fffa74d4ebfba1c36ee9ddda96cc5544cbd3d22d
Anjali <anjaliguptajnp21@gmail.com>
2021-08-18 20:25:52 +0530

    commit

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,14 @@
+import React from "react";
 // declare class
-
+class Shape extends React.Component {
+    constructor(props) {
+        super(props);
+        this.color = "red";
+    }
+    drawShape() {
+    }
+    calculateArea(){
+    }
+}
 // export class using module.exports
+module.exports = Shape;
